19dc41fc9688733f466f00b9abcb1419ca03cebb
Henrik <eaahve@students.eaaa.dk>
2022-02-25 09:45:19 +0100

    DagligFastTest

--- Ordination/src/test/DagligFastTest.java
+++ Ordination/src/test/DagligFastTest.java
@@ -1,15 +1,41 @@
 package test;
 
+import ordination.DagligFast;
+import ordination.Dosis;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 class DagligFastTest {
 
+    DagligFast d = new DagligFast(LocalDate.of(2022, 02, 23),
+            LocalDate.of(2022, 02, 24),1,1,1,1);
+
+    DagligFast d2 = new DagligFast(LocalDate.of(2022, 02, 23),
+            LocalDate.of(2022, 02, 24),1,2,1,0);
     @Test
     void samletDosis() {
+
+        //Arrange
+
+        //Act
+
+        //Assert
+        assertEquals(8,d.samletDosis());
+        assertEquals(8, d2.samletDosis());
     }
 
     @Test
     void doegnDosis() {
+
+        //Arrange
+        DagligFast d2 = new DagligFast(LocalDate.of(2022, 02, 23),
+                LocalDate.of(2022, 02, 24),1,2,1,0);
+assertEquals(4, d2.doegnDosis());
+assertEquals(4, d2.doegnDosis());
     }
 }
